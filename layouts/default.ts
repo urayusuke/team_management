553f8c7b4036be11027fa74129e1a786a47a7c39
urayusuke <blue.yusuke69@gmail.com>
2023-02-07 17:27:55 +0900

    ハンバーガーメニューの作成

--- layouts/default.ts
+++ layouts/default.ts
@@ -4,24 +4,30 @@ export default Vue.extend({
   data () {
     return {
       title: 'TEAM MANAGEMENT',
-      sideBarList: [ 
+      sideBarDrawer: false,
+      sideBarList: [
         {
+          id: 1,
           title: 'お知らせ',
-          icon: 'mdi-bullhorn' 
+          icon: 'mdi-bullhorn'
         },
         {
+          id: 2,
           title: 'メンバー一覧',
-          icon: 'mdi-account-group' 
+          icon: 'mdi-account-group'
         },
         {
+          id: 3,
           title: '出 欠',
-          icon: 'mdi-calendar-month' 
+          icon: 'mdi-calendar-month'
         },
         {
+          id: 4,
           title: 'テーマ設定',
-          icon: 'mdi-wrench-cog-outline' 
+          icon: 'mdi-wrench-cog-outline'
         },
-      ] 
+      ],
+
     }
   },
 })
