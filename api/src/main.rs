bf037efb65714c6638c07b65e24ad9a0ce2be767
浦佑輔 <yusuke@urayuusukenoMacBook-Air-2.local>
2023-04-22 16:47:42 +0900

    docker環境構築 #3 修正

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpResponse, HttpServer};
+
+#[get("/")]
+async fn hello() -> HttpResponse {
+    HttpResponse::Ok().body("Hello World")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(hello))
+        .bind(("0.0.0.0", 8080))?
+        .run()
+        .await
 }
